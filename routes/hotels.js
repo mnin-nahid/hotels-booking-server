24778a51386cc50c5b11843345431b38294fcb94
mnin-nahid <nahidulislam.nayan54@gmail.com>
2022-06-02 02:53:14 +0600

    hotel models addeded

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,6 +1,24 @@
 import express from "express";
+import Hotel from "../models/Hotel.js";
 
 const router = express.Router();
+
+//CREATE
+router.post("/", async(req, res)=>{
+    const newHotel = new Hotel(req.body);
+    
+    try{
+        const savedHotel = await newHotel.save();
+        res.status(200).json(savedHotel);
+
+    }catch(error){
+        res.status(500).json(error);
+    }
+})
+//UPDATE
+//DELETE
+//GET
+//GET ALL
 
 router.get("/", (req, res)=>{
     res.send("Hey, This is hotels endpoint");
